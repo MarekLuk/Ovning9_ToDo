af0c96de52b381f159b57bc904d43d18e9977d43
MarekL-MarekL <markandmark3@gmail.com>
2024-04-03 14:22:10 +0200

    funkra bra nu

--- todo.js
+++ todo.js
@@ -1,7 +1,43 @@
-document.addEventListener("click", addTodo);
-
+const element = document.getElementById("myBtn");
+const todoList = document.getElementById("todoList");
+const todoText = document.getElementById("todoText");
 
 function addTodo() {
-    document.
-    
+	var todoTextValue = todoText.value.trim();
+
+	if (todoTextValue !== "") {
+		const newItem = document.createElement("li");
+		const newContainer = document.createElement("div");
+		newContainer.textContent = todoTextValue;
+
+		newItem.appendChild(newContainer);
+
+		const deleteBtn = document.createElement("button");
+		deleteBtn.innerHTML = "<i class='fa-regular fa-trash-can'></i>";
+		deleteBtn.classList.add("remove-btn");
+		deleteBtn.addEventListener("click", function () {
+			if (confirm("Are you sure to remove item?")) {
+				newItem.remove();
+			}
+		});
+
+		const checkBtn = document.createElement("button");
+		checkBtn.innerHTML = todoText.value =
+			"<i class='fa-regular fa-square-check'></i>";
+		checkBtn.classList.add("check-btn");
+		checkBtn.addEventListener("click", function () {
+			newItem.classList.toggle("task-done");
+		});
+
+		newItem.appendChild(checkBtn);
+		newItem.appendChild(deleteBtn);
+
+		todoList.appendChild(newItem);
+
+		todoText.value = "";
+	} else {
+		alert("todo is empty");
+	}
 }
+
+element.addEventListener("click", addTodo);
